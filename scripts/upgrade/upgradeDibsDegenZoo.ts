a01746947aa261a3833e4c897ee0b59c1721f224
spsina <snparvizi75@gmail.com>
2023-05-06 11:55:49 +0330

    upgarde dibs degenZoo

---
+++ scripts/upgrade/upgradeDibsDegenZoo.ts
@@ -0,0 +1,29 @@
+import hre, { upgrades, ethers } from "hardhat";
+
+async function upgrade(proxyAddress: string) {
+  const DibsDegenZoo = await ethers.getContractFactory("DibsDegenZoo");
+  const dibsDegenZoo = await upgrades.upgradeProxy(proxyAddress, DibsDegenZoo);
+
+  await dibsDegenZoo.deployed();
+
+  console.log("DibsDegenZoo upgraded:", dibsDegenZoo.address);
+
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(
+    dibsDegenZoo.address
+  );
+
+  console.log("Implementation deployed to:", implementationAddress);
+
+  // verify
+  await hre.run("verify:verify", {
+    address: implementationAddress,
+    constructorArguments: [],
+  });
+}
+
+upgrade("0x3fBA73Fc55dd7cb286F963793F5301E92cC07B57")
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
