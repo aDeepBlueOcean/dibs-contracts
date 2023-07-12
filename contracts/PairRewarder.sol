481fa4a1afddaf338cca0378b4d41f42c97e2c92
spsina <snparvizi75@gmail.com>
2023-07-12 11:02:48 +0200

    set top referrers

--- contracts/PairRewarder.sol
+++ contracts/PairRewarder.sol
@@ -6,21 +6,150 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract PairRewarder is AccessControlUpgradeable {
+import "./interfaces/IDibs.sol";
+
+interface IPairRewarder {
+    struct LeaderBoardInfo {
+        uint256 winnersCount;
+        address[] rewardTokens;
+        uint256[][] rewardAmounts;
+    }
+
+    struct LeaderBoardWinners {
+        LeaderBoardInfo info;
+        address[] winners;
+    }
+
+    function leaderBoardInfo() external view returns (LeaderBoardInfo memory);
+
+    function leaderBoardWinners(
+        uint256 day
+    ) external view returns (LeaderBoardWinners memory);
+
+    function userLeaderBoardWins(
+        address user
+    ) external view returns (uint256[] memory);
+}
+
+contract PairRewarder is IPairRewarder, AccessControlUpgradeable {
     address public dibs;
     address public pair;
 
-    function initialize(address _dibs, address _pair) public initializer {
+    LeaderBoardInfo _leaderBoardInfo;
+    mapping(uint256 => LeaderBoardWinners) _leaderBoardWinners; // day => winners
+    mapping(address => uint256[]) _userLeaderBoardWins; // user => days
+
+    mapping(address => uint256) public userLastLeaderBoardClaim; // user => day
+
+    bytes32 public constant SETTER_ROLE = keccak256("SETTER_ROLE");
+
+    event LeaderBoardSet(
+        uint256 winnersCount,
+        address[] rewardTokens,
+        uint256[][] rewardAmounts
+    );
+
+    error InvalidInput();
+    error OnlyMuonInterface();
+    error DayNotOver();
+    error TooManyWinners();
+    error AlreadySet();
+
+    function initialize(
+        address dibs_,
+        address pair_,
+        address admin_,
+        address setter_
+    ) public initializer {
         __AccessControl_init();
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _PairRewarder_init(_dibs, _pair);
+        _setupRole(DEFAULT_ADMIN_ROLE, admin_);
+        _setupRole(SETTER_ROLE, setter_);
+        _PairRewarder_init(dibs_, pair_);
     }
 
     function _PairRewarder_init(
-        address _dibs,
-        address _pair
+        address dibs_,
+        address pair_
     ) internal onlyInitializing {
-        dibs = _dibs;
-        pair = _pair;
+        dibs = dibs_;
+        pair = pair_;
+    }
+
+    function leaderBoardInfo() external view returns (LeaderBoardInfo memory) {
+        return _leaderBoardInfo;
+    }
+
+    function leaderBoardWinners(
+        uint256 day_
+    ) external view override returns (LeaderBoardWinners memory) {
+        return _leaderBoardWinners[day_];
+    }
+
+    function userLeaderBoardWins(
+        address user_
+    ) external view override returns (uint256[] memory) {
+        return _userLeaderBoardWins[user_];
+    }
+
+    function activeDay() public view returns (uint256) {
+        return
+            uint32(
+                (block.timestamp - IDibs(dibs).firstRoundStartTime()) / 1 days
+            );
+    }
+
+    function setTopReferrers(
+        uint256 day,
+        address[] memory winners
+    ) public onlyMuonInterface {
+        if (day >= activeDay()) {
+            revert DayNotOver();
+        }
+
+        if (winners.length > _leaderBoardInfo.winnersCount) {
+            revert TooManyWinners();
+        }
+
+        if (_leaderBoardWinners[day].winners.length > 0) {
+            revert AlreadySet();
+        }
+
+        _leaderBoardWinners[day].winners = winners;
+        _leaderBoardWinners[day].info = _leaderBoardInfo;
+
+        for (uint256 i = 0; i < winners.length; i++) {
+            _userLeaderBoardWins[winners[i]].push(day);
+        }
+    }
+
+    function setLeaderBoard(
+        uint256 winnersCount_,
+        address[] memory rewardTokens_,
+        uint256[][] memory rewardAmounts_
+    ) public onlyRole(SETTER_ROLE) {
+        if (rewardTokens_.length != rewardAmounts_.length) {
+            revert InvalidInput();
+        }
+
+        for (uint256 i = 0; i < rewardTokens_.length; i++) {
+            if (
+                rewardTokens_[i] == address(0) ||
+                rewardAmounts_[i].length != winnersCount_
+            ) {
+                revert InvalidInput();
+            }
+        }
+
+        _leaderBoardInfo.winnersCount = winnersCount_;
+        _leaderBoardInfo.rewardTokens = rewardTokens_;
+        _leaderBoardInfo.rewardAmounts = rewardAmounts_;
+
+        emit LeaderBoardSet(winnersCount_, rewardTokens_, rewardAmounts_);
+    }
+
+    modifier onlyMuonInterface() {
+        if (msg.sender != IDibs(dibs).muonInterface())
+            revert OnlyMuonInterface();
+        _;
     }
 }
--- test/pairRewarder.ts
+++ test/pairRewarder.ts
@@ -7,6 +7,8 @@ import {
 } from "../typechain-types";
 import { MockContract, deployMockContract } from "ethereum-waffle";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { getCurrentTimeStamp } from "./timeUtils";
+import { BigNumber } from "ethers";
 
 describe("PairRewarder", () => {
   let pairRewarder: PairRewarder;
@@ -15,31 +17,399 @@ describe("PairRewarder", () => {
   let rt1: MockContract; // reward token 1
   let rt2: MockContract; // reward token 2
   let admin: SignerWithAddress;
-
-  async function setupPairRewarder() {
-    const PairRewarderFactory = await ethers.getContractFactory("PairRewarder");
-    pairRewarder = (await PairRewarderFactory.deploy()) as PairRewarder;
-    await pairRewarder.deployed();
-  }
+  let setter: SignerWithAddress;
+  let winner1: SignerWithAddress;
+  let winner2: SignerWithAddress;
+  let winner3: SignerWithAddress;
+  let muonInterface: SignerWithAddress;
 
   beforeEach(async () => {
-    [admin] = await ethers.getSigners();
+    [admin, setter, winner1, winner2, winner3, muonInterface] =
+      await ethers.getSigners();
     pair = await deployMockContract(admin, ERC20__factory.abi);
     rt1 = await deployMockContract(admin, ERC20__factory.abi);
     rt2 = await deployMockContract(admin, ERC20__factory.abi);
     dibs = await deployMockContract(admin, Dibs__factory.abi);
 
     const PairRewarderFactory = await ethers.getContractFactory("PairRewarder");
-    const args = [dibs.address, pair.address];
+    const args = [dibs.address, pair.address, admin.address, setter.address];
     pairRewarder = (await upgrades.deployProxy(
       PairRewarderFactory,
       args
     )) as PairRewarder;
     await pairRewarder.deployed();
+
+    await dibs.mock.muonInterface.returns(muonInterface.address);
   });
 
-  it("should have the correct pair and dibs address", async () => {
+  it("should have the correct pair and dibs address and roles", async () => {
     expect(await pairRewarder.pair()).to.equal(pair.address);
     expect(await pairRewarder.dibs()).to.equal(dibs.address);
+
+    const isAdmin = await pairRewarder.hasRole(
+      await pairRewarder.DEFAULT_ADMIN_ROLE(),
+      admin.address
+    );
+    const isSetter = await pairRewarder.hasRole(
+      await pairRewarder.SETTER_ROLE(),
+      setter.address
+    );
+  });
+
+  it("should not be able to set reward tokens if not setter", async () => {
+    const tx = pairRewarder
+      .connect(admin)
+      .setLeaderBoard(1, [rt1.address], [[10]]);
+
+    await expect(tx).to.be.reverted;
+  });
+
+  it("should be able to set reward tokens if setter", async () => {
+    const tx = pairRewarder
+      .connect(setter)
+      .setLeaderBoard(1, [rt1.address], [[10]]);
+    await expect(tx).to.not.be.reverted;
+  });
+
+  it("should fail to set invalid reward tokens and amounts", async () => {
+    // invalid reward tokens and amounts count
+
+    const tx = pairRewarder
+      .connect(setter)
+      .setLeaderBoard(1, [rt1.address, rt2.address], [[10]]);
+
+    await expect(tx).to.be.revertedWithCustomError(
+      pairRewarder,
+      "InvalidInput"
+    );
+
+    // invalid reward tokens and winners count
+
+    const tx2 = pairRewarder
+      .connect(setter)
+      .setLeaderBoard(2, [rt1.address], [[10]]);
+    await expect(tx2).to.be.revertedWithCustomError(
+      pairRewarder,
+      "InvalidInput"
+    );
+  });
+
+  it("should set reward tokens correctly", async () => {
+    await pairRewarder.connect(setter).setLeaderBoard(1, [rt1.address], [[10]]);
+
+    const leaderBoardInfo = await pairRewarder.leaderBoardInfo();
+
+    expect(leaderBoardInfo.winnersCount).to.equal(1);
+    expect(leaderBoardInfo.rewardTokens).to.deep.equal([rt1.address]);
+    expect(leaderBoardInfo.rewardAmounts).to.deep.equal([[10]]);
+
+    await pairRewarder.connect(setter).setLeaderBoard(
+      2,
+      [rt1.address, rt2.address],
+      [
+        [10, 5],
+        [20, 10],
+      ]
+    );
+    const leaderBoardInfo2 = await pairRewarder.leaderBoardInfo();
+
+    expect(leaderBoardInfo2.winnersCount).to.equal(2);
+    expect(leaderBoardInfo2.rewardTokens).to.deep.equal([
+      rt1.address,
+      rt2.address,
+    ]);
+    expect(leaderBoardInfo2.rewardAmounts).to.deep.equal([
+      [10, 5],
+      [20, 10],
+    ]);
+  });
+
+  it("should not be able to set top referrers if not muon interface", async () => {
+    const tx = pairRewarder.setTopReferrers(0, [winner1.address]);
+
+    await expect(tx).to.be.revertedWithCustomError(
+      pairRewarder,
+      "OnlyMuonInterface"
+    );
+  });
+
+  it("should fail to set top referrers if day not over", async () => {
+    const now = await getCurrentTimeStamp();
+
+    await dibs.mock.firstRoundStartTime.returns(now);
+
+    const tx = pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(BigNumber.from(5), [winner1.address]);
+
+    await expect(tx).to.be.revertedWithCustomError(pairRewarder, "DayNotOver");
+  });
+
+  it("should revert if top referrers count is more than winners count", async () => {
+    const now = await getCurrentTimeStamp();
+
+    await dibs.mock.firstRoundStartTime.returns(now);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder
+      .connect(setter)
+      .setLeaderBoard(2, [rt1.address], [[10, 5]]);
+
+    const tx = pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(BigNumber.from(0), [
+        winner1.address,
+        winner2.address,
+        winner3.address,
+      ]);
+
+    await expect(tx).to.be.revertedWithCustomError(
+      pairRewarder,
+      "TooManyWinners"
+    );
+  });
+
+  it("should set top referrers correctly", async () => {
+    const now = await getCurrentTimeStamp();
+
+    await dibs.mock.firstRoundStartTime.returns(now);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder
+      .connect(setter)
+      .setLeaderBoard(2, [rt1.address], [[10, 5]]);
+
+    await pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(0, [winner1.address, winner2.address]);
+
+    const day0winners = await pairRewarder.leaderBoardWinners(0);
+    const winner1wins = await pairRewarder.userLeaderBoardWins(winner1.address);
+    const winner2wins = await pairRewarder.userLeaderBoardWins(winner2.address);
+
+    expect(day0winners.winners).to.deep.equal([
+      winner1.address,
+      winner2.address,
+    ]);
+
+    expect(day0winners.info.winnersCount).to.equal(2);
+    expect(day0winners.info.rewardTokens).to.deep.equal([rt1.address]);
+    expect(day0winners.info.rewardAmounts).to.deep.equal([[10, 5]]);
+    expect(winner1wins).to.deep.equal([0]);
+    expect(winner2wins).to.deep.equal([0]);
+  });
+
+  it("should revert if top referrers already set", async () => {
+    const now = await getCurrentTimeStamp();
+
+    await dibs.mock.firstRoundStartTime.returns(now);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder
+      .connect(setter)
+      .setLeaderBoard(2, [rt1.address], [[10, 5]]);
+
+    await pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(0, [winner1.address, winner2.address]);
+
+    const tx = pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(0, [winner1.address, winner2.address]);
+
+    await expect(tx).to.be.revertedWithCustomError(pairRewarder, "AlreadySet");
+  });
+
+  it("should correctly set winner on multiple days", async () => {
+    const now = await getCurrentTimeStamp();
+
+    await dibs.mock.firstRoundStartTime.returns(now);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder
+      .connect(setter)
+      .setLeaderBoard(2, [rt1.address], [[10, 5]]);
+
+    await pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(0, [winner1.address, winner2.address]);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(1, [winner2.address, winner3.address]);
+
+    const day0winners = await pairRewarder.leaderBoardWinners(0);
+    const day1winners = await pairRewarder.leaderBoardWinners(1);
+
+    const winner1wins = await pairRewarder.userLeaderBoardWins(winner1.address);
+    const winner2wins = await pairRewarder.userLeaderBoardWins(winner2.address);
+    const winner3wins = await pairRewarder.userLeaderBoardWins(winner3.address);
+
+    expect(day0winners.winners).to.deep.equal([
+      winner1.address,
+      winner2.address,
+    ]);
+    expect(day1winners.winners).to.deep.equal([
+      winner2.address,
+      winner3.address,
+    ]);
+
+    expect(day0winners.info.winnersCount).to.equal(2);
+    expect(day0winners.info.rewardTokens).to.deep.equal([rt1.address]);
+    expect(day0winners.info.rewardAmounts).to.deep.equal([[10, 5]]);
+    expect(day1winners.info.winnersCount).to.equal(
+      2,
+      "day 1 winners count should be 2"
+    );
+    expect(day1winners.info.rewardTokens).to.deep.equal([rt1.address]);
+    expect(day1winners.info.rewardAmounts).to.deep.equal([[10, 5]]);
+    expect(winner1wins).to.deep.equal([0]);
+    expect(winner2wins).to.deep.equal([0, 1]);
+    expect(winner3wins).to.deep.equal([1]);
+  });
+
+  it("should set correct winner on multiple days with multiple reward tokens", async () => {
+    const now = await getCurrentTimeStamp();
+
+    await dibs.mock.firstRoundStartTime.returns(now);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder.connect(setter).setLeaderBoard(
+      2,
+      [rt1.address, rt2.address],
+      [
+        [10, 5],
+        [20, 10],
+      ]
+    );
+
+    await pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(0, [winner1.address, winner2.address]);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(1, [winner2.address, winner3.address]);
+
+    const day0winners = await pairRewarder.leaderBoardWinners(0);
+    const day1winners = await pairRewarder.leaderBoardWinners(1);
+
+    const winner1wins = await pairRewarder.userLeaderBoardWins(winner1.address);
+    const winner2wins = await pairRewarder.userLeaderBoardWins(winner2.address);
+    const winner3wins = await pairRewarder.userLeaderBoardWins(winner3.address);
+
+    expect(day0winners.winners).to.deep.equal([
+      winner1.address,
+      winner2.address,
+    ]);
+
+    expect(day1winners.winners).to.deep.equal([
+      winner2.address,
+      winner3.address,
+    ]);
+
+    expect(day0winners.info.winnersCount).to.equal(2);
+    expect(day0winners.info.rewardTokens).to.deep.equal([
+      rt1.address,
+      rt2.address,
+    ]);
+    expect(day0winners.info.rewardAmounts).to.deep.equal([
+      [10, 5],
+      [20, 10],
+    ]);
+
+    expect(day1winners.info.winnersCount).to.equal(2);
+    expect(day1winners.info.rewardTokens).to.deep.equal([
+      rt1.address,
+      rt2.address,
+    ]);
+    expect(day1winners.info.rewardAmounts).to.deep.equal([
+      [10, 5],
+      [20, 10],
+    ]);
+
+    expect(winner1wins).to.deep.equal([0]);
+    expect(winner2wins).to.deep.equal([0, 1]);
+    expect(winner3wins).to.deep.equal([1]);
+  });
+
+  it("should set correct winner on multiple days where rewards are different", async () => {
+    const now = await getCurrentTimeStamp();
+
+    await dibs.mock.firstRoundStartTime.returns(now);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder.connect(setter).setLeaderBoard(
+      2,
+      [rt1.address, rt2.address],
+      [
+        [10, 5],
+        [20, 10],
+      ]
+    );
+
+    await pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(0, [winner1.address, winner2.address]);
+
+    // move time to next day
+    await ethers.provider.send("evm_increaseTime", [86400]);
+
+    await pairRewarder
+      .connect(setter)
+      .setLeaderBoard(2, [rt1.address], [[10, 5]]);
+
+    await pairRewarder
+      .connect(muonInterface)
+      .setTopReferrers(1, [winner2.address, winner3.address]);
+
+    const day0winners = await pairRewarder.leaderBoardWinners(0);
+    const day1winners = await pairRewarder.leaderBoardWinners(1);
+
+    const winner1wins = await pairRewarder.userLeaderBoardWins(winner1.address);
+    const winner2wins = await pairRewarder.userLeaderBoardWins(winner2.address);
+    const winner3wins = await pairRewarder.userLeaderBoardWins(winner3.address);
+
+    expect(day0winners.winners).to.deep.equal([
+      winner1.address,
+      winner2.address,
+    ]);
+    expect(day1winners.winners).to.deep.equal([
+      winner2.address,
+      winner3.address,
+    ]);
+    expect(day0winners.info.winnersCount).to.equal(2);
+    expect(day0winners.info.rewardTokens).to.deep.equal([
+      rt1.address,
+      rt2.address,
+    ]);
+    expect(day0winners.info.rewardAmounts).to.deep.equal([
+      [10, 5],
+      [20, 10],
+    ]);
+    expect(day1winners.info.winnersCount).to.equal(2);
+    expect(day1winners.info.rewardTokens).to.deep.equal([rt1.address]);
+    expect(day1winners.info.rewardAmounts).to.deep.equal([[10, 5]]);
+    expect(winner1wins).to.deep.equal([0]);
+    expect(winner2wins).to.deep.equal([0, 1]);
+    expect(winner3wins).to.deep.equal([1]);
   });
 });
